86e61faa775a6600766603f50ded6caf9fb2dc03
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-12 01:12:53 +0800

    change struct node to typedef node

--- linklist.c
+++ linklist.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+typedef struct NODE {
 	char data;
-	struct node *next;
-};
+	struct NODE *next;
+}node;
 
 void main() 
 {
-	struct node *head, *current, *n1, *n2, *n3;
+	node *head, *current, *n1, *n2, *n3;
 
-	n1 = (struct node *)malloc(sizeof(struct node));
+	n1 = (node* )malloc(sizeof(node));
 	head->next = n1;
 	n1->data = 'a';
 	//n1->next = NULL;
 	
-	n2 = (struct node *)malloc(sizeof(struct node));
+	n2 = (node* )malloc(sizeof(node));
         n1->next = n2;
         n2->data = 'b';
         //n2->next = NULL;
 
-	n3 = (struct node *)malloc(sizeof(struct node));
+	n3 = (node* )malloc(sizeof(node));
         n2->next = n3;
         n3->data = 'c';
         n3->next = NULL;
