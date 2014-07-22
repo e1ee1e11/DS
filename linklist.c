d30fbab2434b34cd0f4a9627406841c9153e77de
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-22 23:39:20 +0800

    add comment for all codes

--- linklist.c
+++ linklist.c
@@ -11,25 +11,31 @@ void insert_node(node*, node*);
 void print_list(node* );
 
 void main() 
-{
+{	
+	//create node n1~n3 & list
 	node *list = create_node('\0');
 	node *n1 = create_node('a');
 	node *n2 = create_node('b');
 	node *n3 = create_node('c');
 	
+	//list->n1->n2->n3
 	insert_node(list, n1);
 	insert_node(n1, n2);
 	insert_node(n2, n3);
 	print_list(list);
 	
+	//create node n4~n6 & list1
 	node *list1 = create_node('\0');
 	node *n4 = create_node('d');
         node *n5 = create_node('e');
         node *n6 = create_node('f');
+	
+	//list1->n4->n5->n6
 	insert_node(list1, n4);
 	insert_node(n4, n5);
         insert_node(n5, n6);
-	n6->next = n1;
+
+	//print link list
 	print_list(list1);
 	print_list(list);
 }
